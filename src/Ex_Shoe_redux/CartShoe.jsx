e57b791402a9efcd2b573020ecd6e2d81ba1cd9c
Dang <huynhdang1707@gmail.com>
2023-12-01 00:09:10 +0700

    update

--- src/Ex_Shoe_redux/CartShoe.jsx
+++ src/Ex_Shoe_redux/CartShoe.jsx
@@ -2,8 +2,30 @@ import React, { Component } from 'react'
 
 export default class CartShoe extends Component {
   render() {
-    return (
-      <div>CartShoe</div>
-    )
+    return <div className='col-6'>
+        <div className='table'>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Img</th>
+                    <th>Quantity</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.props.cart.map((item)=>{
+                    return(
+                        <tr>
+                            <td></td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </div>
+
+    </div>
+      
+    
   }
 }
--- src/Ex_Shoe_redux/ListShoe.jsx
+++ src/Ex_Shoe_redux/ListShoe.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+import ItemShoe from './ItemShoe'
+import { connect } from 'react-redux'
 
-export default class ListShoe extends Component {
+ class ListShoe extends Component {
   render() {
+    let {list} = this.props
     return (
-      <div>ListShoe</div>
+      <div className='row col-6'>{list.map((item)=>{
+        return <ItemShoe key={item.id} data={item}/>
+      })}</div>
     )
   }
 }
+
+
+let mapStateToProps = (state) =>{
+    return{
+        list : state.shoeArr
+    }
+}
+
+
+
+
+export default connect(mapStateToProps)(ListShoe);
